873d0665395a98ee0f41d0b170594d32f451683e
hoang_son <vihoangson@gmail.com>
2018-09-14 06:23:11 +0700

    update unitest

--- app/Http/Controllers/GyazoController.php
+++ app/Http/Controllers/GyazoController.php
@@ -21,6 +21,7 @@ class GyazoController extends Controller
         $file_path = public_path('/storage/images/Gyazo/' . $name);
         if (file_exists($file_path)) {
             $this->CloudinaryUploadImg($file_path, 'Gyazo');
+            unlink($file_path);
             echo env('APP_URL').'/upload?file=' . base64_encode($name);
         } else {
             echo "Can't update file";
