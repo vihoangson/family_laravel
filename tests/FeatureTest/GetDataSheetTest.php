cb6e568b16089930e6872b8f5459e4175bdfe409
hoang_son <vihoangson@gmail.com>
2018-09-24 21:38:09 +0700

    update unitest

---
+++ tests/FeatureTest/GetDataSheetTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class GetDataSheetTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $spreadsheet_url = "https://docs.google.com/spreadsheets/d/1t5RVjamu-L16f19w27Q-yBCrLtk-sshWevda2ZhlF3U/export?format=csv&gid=0";
+        $data = $this->getdatasheet($spreadsheet_url);
+        foreach ($data as $value){
+            if($value['Assign']=='Anh Tuấn' && $value['Progress'] == '100%'){
+                dump($value);
+            }
+        }
+        $this->assertGreaterThan(1,count($data));
+    }
+
+    function getdatasheet($spreadsheet_url){
+
+        if (!ini_set('default_socket_timeout', 15)) {
+            echo "<!-- unable to change socket timeout -->";
+        }
+
+        if (($handle = fopen($spreadsheet_url, "r")) !== false) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                $spreadsheet_data[] = $data;
+            }
+            fclose($handle);
+
+            foreach ($spreadsheet_data[0] as $key => $value) {
+                $name_column [] = $value;
+            }
+            $return = [];
+            foreach ($spreadsheet_data as $key => $value) {
+                if ($key == 0) {
+                    continue;
+                }
+                foreach ($value as $k => $v) {
+                    $return[$key][$name_column[$k]] = $v;
+                }
+            }
+
+
+        } else {
+            die("Problem reading csv");
+        }
+        return $return;
+    }
+}
