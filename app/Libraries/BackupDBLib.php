11cd2014c386691f2b172be20c4f52159f4e15ab
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2019-01-09 10:20:23 +0700

    Fix bug backup

--- app/Libraries/BackupDBLib.php
+++ app/Libraries/BackupDBLib.php
@@ -10,7 +10,12 @@ class BackupDBLib {
         }
 
         try {
-            $url_page = explode("//", env('APP_URL'));
+            if (preg_match("/:\/\//", env('APP_URL'))) {
+                $url_page = explode("//", env('APP_URL'));
+            } else {
+                $url_page = env('APP_URL');
+            }
+
             $new_file = $url_page[1] . '_data_family.' . date("Y-m-d__H_i_s") . '.bk';
 
             copy(($file), base_path('sqlite/' . $new_file));
