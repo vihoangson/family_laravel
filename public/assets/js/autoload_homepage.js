94b7092fda2f9469973dfc6200b8a56d6d3d2b0d
vihoangson <vihoangson@gmail.com>
2019-02-08 07:51:58 +0700

    avatar

--- public/assets/js/autoload_homepage.js
+++ public/assets/js/autoload_homepage.js
@@ -16,7 +16,7 @@ var loadKyniem = function () {
                         mmm.find('.block-title').html('Happy Family');
                     }
 
-                    if((typeof v.user.avatar !== 'undefined')){
+                    if((v.user.hasOwnProperty('avatar'))){
                         mmm.find('.img-avatar').attr('src', v.user.avatar);
                     }
 
